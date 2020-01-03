02c5072831c3815af4b61c845e68ae0962b52d35
Yago <yagowns@gmail.com>
2020-01-03 20:34:26 +0000

    Add files via upload

--- script/script.js
+++ script/script.js
@@ -1,1 +1,12 @@
-a
+//lista de gifs possiveis a carregar na homepage
+var showcaseArray = [
+	'img/1.gif',
+	'img/2.gif',
+	'img/3.webp',
+	'img/4.gif',
+];
+
+$(document).ready(function(){
+    var randomShowcase = showcaseArray[Math.floor(Math.random()*showcaseArray.length)];
+    $('#showcase').attr({ src: randomShowcase});
+});
